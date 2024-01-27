d22f44409f6a473df3b9c63d46ea8b372f246be9
fatemeh-khodabakhsh <157875212+fatemeh-khodabakhsh@users.noreply.github.com>
2024-01-27 23:26:47 +0330

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -1,4 +1,3 @@
-h = []
 def binbasen(x, y):
     t = ""
     while x > 0:
@@ -6,19 +5,18 @@ def binbasen(x, y):
         t = str(f)+t
         x //= y
     return t
+h = []    
 while True:
     a, b = map(int, input().split())
     if [a, b] == [-1, -1]:
         break
     else:
-        sum = 0
+        total_sum = 0
         if 2 <= b <= 9:
             for i in range(1, a + 1):
                 if a % i == 0:
-                    sum += i
-            h.append(binbasen(sum,b))
-
-
+                    total_sum += i
+            h.append(binbasen(total_sum,b))
         else:
             print("invalid base!")
             exit()
@@ -26,6 +24,7 @@ p = 0
 for i in h:
     p += int(i)
 print(p)
+
 
 
 
