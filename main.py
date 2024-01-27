073824f6f6e5a7b994bbac9876f78db774327fb3
fatemeh-khodabakhsh <157875212+fatemeh-khodabakhsh@users.noreply.github.com>
2024-01-27 23:23:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+def clean_pattern(n):
+    pattern = [["." for i in range(n)]]
+    pattern[0][0] = "*"
+    a, b = 0, 0
+
+    while True:
+        m = input()
+        if m == "END":
+            break
+        if m == "R":
+            b = min(b+1, n-1)
+        elif m == "L":
+            b = max(b-1, 0)
+        elif m == "B":
+            a += 1
+        if a == len(pattern):
+            pattern.append(["."for i in range(n)])
+        pattern[a][b] = "*"
+    for row in pattern:
+        print(" ".join(row))
+    if a != len(pattern)-1 or b != n-1:
+        print("There's no way out!")
+
 n = int(input())
-pattern = [["." for i in range(n)]]
-pattern[0][0] = "*"
-a, b = 0, 0
-
-while True:
-    m = input()
-    if m == "END":
-        break
-    if m == "R":
-        b = min(b+1, n-1)
-    elif m == "L":
-        b = max(b-1, 0)
-    elif m == "B":
-        a += 1
-    if a == len(pattern):
-        pattern.append(["."for i in range(n)])
-    pattern[a][b] = "*"
-for row in pattern:
-    print(" ".join(row))
-if a != len(pattern)-1 or b != n-1:
-    print("There's no way out!")
-
+clean_pattern(n)
 
 
 
