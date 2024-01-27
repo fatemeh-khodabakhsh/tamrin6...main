aaf40040be87c497fa162913a8d6a29280f3b78d
fatemeh-khodabakhsh <157875212+fatemeh-khodabakhsh@users.noreply.github.com>
2024-01-27 23:29:05 +0330

    Update main (3).py

--- main (3).py
+++ main (3).py
@@ -1,12 +1,12 @@
 n = int(input())
-temp =1
 for i in range(n):
     for j in range(1 , n-i+1):
         print(end='')
+    temp = 1   
     for j in range(i+1):
-        if (j==0 or i==j):
+        if (j == 0 or i == j):
             temp = 1
         else:
-            temp = temp*(i-j+1)//j
+            temp = temp * (i-j+1) // j
         print(temp, end=" ")
     print()
